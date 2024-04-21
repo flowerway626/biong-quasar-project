72a1146e7dd78b177ba60fa0ed704cc034825922
flowerway626 <flowerway626@gmail.com>
2024-04-21 11:57:11 +0000

    Deploying to gh-pages from @ flowerway626/biong-front@1e59329dc5f91d6855ca37d2ed868e11601c9a9a 🚀

---
+++ assets/QCardActions.d19de371.js
@@ -0,0 +1,1 @@
+import{B as t,bk as r,bl as c,c as l,h as n,D as i}from"./index.f0595662.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=l(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>n("div",{class:o.value},i(s.default))}});export{d as Q};
---
+++ assets/swiper.fc13efc0.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.f0595662.js";import{S as a,a as i}from"./swiper-slide.88ba484f.js";/* empty css                          */var f=e(({app:r})=>{r.use(a),r.use(i)});export{f as default};
